ef1c9e0bd9ad8da53f8614ac21593b60424d986f
dagori <mariyav@protonmail.com>
2019-09-01 15:16:10 +0300

    test

--- js/script.js
+++ js/script.js
@@ -1,67 +1,13 @@
 'use strict';
-const slider = document.querySelector('.slider');
+
+window.slider = document.querySelector('.slider');
+
 const vertical = document.querySelector('.slider__vertical-container');
-const horizontal = document.querySelector('.slider__horizontal-container');
 const sliderVerticalWrapper = document.querySelector('.slider__wrapper');
 const vericalSlides = vertical.querySelectorAll('.slide');
 
 const radio = document.querySelector('.switcher-radio');
 const radioItem = radio.querySelectorAll('input');
-
-const scale = document.querySelector('.switcher-slider__scale');
-const thumb = document.querySelector('.switcher-slider__thumb');
-
-horizontal.style.width = (parseFloat(getComputedStyle(slider).width) * horizontal.children.length) + 'px';
-
-document.ondragstart = function() {
-  return false;
-};
-//Получить координаты элемента
-function getCoords(elem) {
-  var coords = elem.getBoundingClientRect();
-  return {
-    left: coords.left + pageXOffset,
-    top: coords.top + pageYOffset,
-    right: coords.right + pageXOffset,
-    bottom: coords.bottom + pageYOffset
-  }
-}
-
-// Передвинуть горизонтальный слайдер
-function moveHorizontalSlides() {
-  var currentPos = parseFloat(getComputedStyle(thumb).left);
-  var width = scale.offsetWidth;
-  var step = slider.offsetWidth;
-  var division = width/4;
-  return (function() {
-    if(currentPos > division) {
-      horizontal.style.transform = `translateX(-${ step}px)`;
-    }
-    if(currentPos > width - division) {
-      horizontal.style.transform = `translateX(-${step + step}px)`;
-    }
-    if(currentPos < division) {
-      horizontal.style.transform = `translateX(0px)`;
-    }
-  })();
-}
-
-thumb.onmousedown = function(e) {//handleStart, handleEnd
-  var thumbPos = getCoords(thumb);
-  var scalePos = getCoords(scale);
-  var shiftX = e.pageX - thumbPos.left;
-  thumb.style.cursor = 'grabbing';
-  document.onmousemove = function(e) {
-    var newPos =  e.pageX - scalePos.left - shiftX;
-    thumb.style.left = Math.max(0, Math.min(newPos, scale.offsetWidth - thumb.offsetWidth)) + 'px';
-    moveHorizontalSlides();
-  }
-  document.onmouseup = function(e) {
-    e.stopPropagation(e);
-    document.onmousemove = null;
-    thumb.style.cursor = 'grab';
-  }
-}
 
 //Передвинуть вертикальный слайдер
 var indexFrom = 0;
@@ -115,12 +61,12 @@ sliderVerticalWrapper.addEventListener('touchstart', function(e){
   e.preventDefault();
   e.stopPropagation();
   start = e.touches[0].pageY;
-}
+}, true);
 sliderVerticalWrapper.addEventListener('touchmove', function(e){
   e.preventDefault();
   e.stopPropagation();
   diff = start-e.touches[0].pageY;
-});
+}, true);
 sliderVerticalWrapper.addEventListener('touchend', function(e){
   e.preventDefault();
   e.stopPropagation();
@@ -134,4 +80,4 @@ sliderVerticalWrapper.addEventListener('touchend', function(e){
     radioItem[indexFrom + 1].checked = true;
     moveVerticalSlides(indexFrom + 1);
   }
-});
+}, true);
---
+++ js/switcher-slider.js
@@ -0,0 +1,58 @@
+const horizontal = document.querySelector('.slider__horizontal-container');
+const scale = document.querySelector('.switcher-slider__scale');
+const thumb = document.querySelector('.switcher-slider__thumb');
+horizontal.style.width = (parseFloat(getComputedStyle(window.slider).width) * horizontal.children.length) + 'px';
+
+document.ondragstart = function() {
+  return false;
+};
+
+//Получить координаты элемента
+function getCoords(elem) {
+  var coords = elem.getBoundingClientRect();
+  return {
+    left: coords.left + pageXOffset,
+    top: coords.top + pageYOffset,
+    right: coords.right + pageXOffset,
+    bottom: coords.bottom + pageYOffset
+  }
+}
+
+// Передвинуть горизонтальный слайдер
+function moveHorizontalSlides() {
+  var currentPos = parseFloat(getComputedStyle(thumb).left);
+  var width = scale.offsetWidth;
+  var step = window.slider.offsetWidth;
+  var division = width/4;
+  return (function() {
+    if(currentPos > division) {
+      horizontal.style.transform = `translateX(-${ step}px)`;
+    }
+    if(currentPos > width - division) {
+      horizontal.style.transform = `translateX(-${step + step}px)`;
+    }
+    if(currentPos < division) {
+      horizontal.style.transform = `translateX(0px)`;
+    }
+  })();
+}
+
+thumb.onmousedown = function(e) {
+  var thumbPos = getCoords(thumb);
+  var scalePos = getCoords(scale);
+  var shiftX = e.pageX - thumbPos.left;
+  thumb.style.cursor = 'grabbing';
+  document.onmousemove = function(e) {
+    var newPos =  e.pageX - scalePos.left - shiftX;
+    thumb.style.left = Math.max(0, Math.min(newPos, scale.offsetWidth - thumb.offsetWidth)) + 'px';
+    moveHorizontalSlides();
+  }
+  document.addEventListener('mouseup', mouseupHandler, true);
+}
+
+function mouseupHandler(e) {
+  e.stopPropagation(e);
+  thumb.style.cursor = 'grab';
+  document.onmousemove = null;
+  document.removeEventListener('mouseup', mouseupHandler, true);
+}
