5a25328cf8a6c3158da2760f638e55f5cb150b0d
dagori <mariyav@protonmail.com>
2019-09-21 12:40:00 +0300

    test2

--- js/webp-remove.js
+++ js/webp-remove.js
@@ -1,5 +1,10 @@
 'use strict';
 (function() {
   const user = window.navigator.userAgent;
-  alert(user);
+  if(user.indexOf('Mac OS') !== -1 && user.indexOf('Safari') !== -1) {
+    const wepb = document.querySelectorAll('.webp');
+    for(let i = 0; i < webp.length; i++) {
+      webp[i].classList.add('no-webp');
+    }
+  }
 })();
